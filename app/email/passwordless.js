ef1f1f2d90b002846eea2898d9894f39eb3a7149
Stephen Bussey <steve.bussey@salesloft.com>
2016-04-15 22:04:59 -0400

    The front-end requires # in routes

--- app/email/passwordless.js
+++ app/email/passwordless.js
@@ -4,7 +4,7 @@ const transporter = nodemailer.createTransport(process.env.SMTP_TRANSPORTER);
 const appUrl = process.env.APPLICATION_BASE_URL;
 
 module.exports = function sendMail(tokenToSend, uidToSend, recipient, callback) {
-  let url = `${appUrl}/auth?token=${tokenToSend}&uid=${uidToSend}`;
+  let url = `${appUrl}#/auth?token=${tokenToSend}&uid=${uidToSend}`;
   var mailOptions = {
       from: '"Reflectives" <noreply@reflectives.com>',
       to: recipient,
