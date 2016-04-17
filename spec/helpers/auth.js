7f6c759f4e710c209d52d495dc0c919eaecebbfc
Stephen Bussey <steve.bussey@salesloft.com>
2016-04-16 22:47:35 -0400

    optionally allow a user override for a token

--- spec/helpers/auth.js
+++ spec/helpers/auth.js
@@ -5,12 +5,12 @@ const co = require('co');
 const db = require('app/models');
 
 module.exports = {
-  getToken: co.wrap(function*() {
-    let user = yield db.user.create({ email: Math.random() + '@test.com' });
+  getToken: co.wrap(function*(user) {
+    user = user || (yield db.user.create({ email: Math.random() + '@test.com' }));
     return jwt.sign({ uid: user.get('id') }, 'test');
   }),
-  getExpiredToken: co.wrap(function*() {
-    let user = yield db.user.create({ email: Math.random() + '@test.com' });
+  getExpiredToken: co.wrap(function*(user) {
+    user = user || (yield db.user.create({ email: Math.random() + '@test.com' }));
     return jwt.sign({ uid: user.get('id') }, 'test', { expiresIn: -1 });
   })
 };
